3f42234b33fc63ef1deaa671132f247055d823bf
sunoru <s@sunoru.com>
2018-05-20 22:52:36 +0800

    Stage manager and player controller.

---
+++ Assets/Scripts/Menu/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+    private void Start()
+    {
+        //Screen.SetResolution(1280, 720, Screen.fullScreen);
+    }
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Stage 1");
+    }
+}
